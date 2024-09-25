1382833accb015310cd8535d6c35f85d17ff9339
Akhilbisht798 <akhilbisht798@gmail.com>
2024-09-25 13:24:13 +0530

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+"use client";
+
+import { NavbarMinimal } from '@/components/Navbar/NavbarMinimal';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <NavbarMinimal />
     </>
   );
 }
